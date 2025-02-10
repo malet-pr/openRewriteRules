d0ee073fe4227f77871e73b96e3b6be9e385de67
Nuria Malet Quintar <maletq.pr@gmail.com>
2025-02-10 20:23:05 -0300

    new attempt at building recipe

---
+++ src/main/java/org/acme/recipes/ChangeLibrariesInJSP.java
@@ -0,0 +1,67 @@
+package org.acme.recipes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
+import org.openrewrite.*;
+import org.openrewrite.xml.*;
+import org.openrewrite.xml.tree.Xml;
+import java.util.*;
+
+public class ChangeLibrariesInJSP extends Recipe {
+    private final List<ResourceMapping> mappings;
+
+    public ChangeLibrariesInJSP(List<ResourceMapping> mappings) {
+        this.mappings = mappings;
+    }
+
+    @Override
+    public @NotNull String getDisplayName() {
+        return "Replace third-party library resource tags";
+    }
+
+    @Override
+    public @NotNull String getDescription() {
+        return "Replace webjars and JSTL-based library imports with direct resource paths";
+    }
+
+    @Override
+    public @NotNull TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new LibraryResourceVisitor(mappings);
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class ResourceMapping {
+        private String libraryName;
+        private String oldPath;
+        private String newPath;
+
+    }
+
+    private static class LibraryResourceVisitor extends XmlVisitor<ExecutionContext> {
+        private final List<ResourceMapping> mappings;
+
+        public LibraryResourceVisitor(List<ResourceMapping> mappings) {
+            this.mappings = mappings;
+        }
+
+        @Override
+        public Xml visitTag(Xml.Tag tag, ExecutionContext ctx) {
+            String currentMarkup = tag.printTrimmed(getCursor());  // needs a Cursor object
+            for (ResourceMapping mapping : mappings) {
+                if (currentMarkup.contains(mapping.getOldPath())) {
+                    try {
+                        return Xml.Tag.build(mapping.getNewPath());
+                    } catch (Exception e) {
+                        ctx.getOnError().accept(e);
+                        return tag;
+                    }
+                }
+            }
+            return super.visitTag(tag, ctx);
+        }
+    }
+}
--- src/main/java/org/acme/recipes/package-info.java
+++
@@ -1,5 +0,0 @@
-@NullMarked
-@NonNullFields
-package org.acme.recipes;
-import org.jspecify.annotations.NullMarked;
-import org.openrewrite.internal.lang.NonNullFields;
