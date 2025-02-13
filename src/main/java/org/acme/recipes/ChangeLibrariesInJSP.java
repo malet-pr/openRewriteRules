bede77037607e6ffb7bca4d9c3a5ce7d38d66561
Nuria Malet Quintar <maletq.pr@gmail.com>
2025-02-12 23:00:22 -0300

    recipe working for one link

--- src/main/java/org/acme/recipes/ChangeLibrariesInJSP.java
+++ src/main/java/org/acme/recipes/ChangeLibrariesInJSP.java
@@ -1,169 +1,88 @@
 package org.acme.recipes;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import org.openrewrite.*;
-import org.openrewrite.xml.*;
-import org.openrewrite.xml.tree.Xml;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.SourceFile;
+import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.text.PlainTextVisitor;
+import org.openrewrite.text.PlainText;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-@Getter
-@Setter
-@AllArgsConstructor
 public class ChangeLibrariesInJSP extends Recipe {
-
-    static String libraryName;
-    static String oldUrl;
-    static String newUrl;
 
     @Override
     public String getDisplayName() {
-        return "Replace third-party library resource tags";
+        return "Change library references in JSP files";
     }
 
     @Override
     public String getDescription() {
-        return "Replace webjars and JSTL-based library imports with direct resource paths";
+        return "Updates library references in JSP files only";
     }
 
     @Override
-    public TreeVisitor<?, ExecutionContext> getVisitor() {
-        XmlVisitor<ExecutionContext> xmlVisitor = new XmlVisitor<ExecutionContext>() {
-            @Override
-            public boolean isAcceptable(SourceFile sourceFile, ExecutionContext ctx) {
-                String path = sourceFile.getSourcePath().toString();
-                boolean isJsp = path.endsWith(".jsp");
-                boolean isInWebInf = path.contains("WEB-INF");
-                System.out.println("Checking file: " + path + " (isJsp: " + isJsp + ", isInWebInf: " + isInWebInf + ")");
-                return isJsp && isInWebInf;
-            }
-            @Override
-            public Xml visitTag(Xml.Tag tag, ExecutionContext ctx) {
-                libraryName = "bootstrap.css";
-                oldUrl = "<link rel=\"stylesheet\" href=\"<c:url value='webjars/bootstrap/3.3.7-1/css/bootstrap.min.css'/>\" />;";
-                newUrl = "<link rel=\"stylesheet\" type=\"text/css\" href=\"resources/librerias/bootstrap/3.3.7-1/css/bootstrap.min.css\" />;";
-                System.out.println("***********************************************************************");
-                System.out.println("Visiting tag: " + tag.getName() + " in " + getClass().getSimpleName());
-                String currentMarkup = "";
-                System.out.println("tag.getContent(): " + tag.getContent());
-                // Only process <link> tags
-                if (!"link".equals(tag.getName())) {
-                    return super.visitTag(tag, ctx);
-                }
-                // Get the href attribute
-                Optional<Xml.Attribute> hrefAttr = findAttribute(tag, "href");
-                if (!hrefAttr.isPresent()) {
-                    return super.visitTag(tag, ctx);
-                }
-                String href = hrefAttr.get().getValueAsString();
-                System.out.println("Found href: " + href);
-                // Check if this is our target library
-                if (!href.contains(libraryName)) {
-                    return super.visitTag(tag, ctx);
-                }
-                System.out.println("Found matching library reference");
-                if (tag.getContent().contains("href") && tag.getContent().contains(libraryName)) {
-                    currentMarkup = tag.getContent().toString();
-                    System.out.println("currentMarkup: " + currentMarkup.toString());
-                    System.out.println("Modifying tag...");
-                    if (!currentMarkup.contains(oldUrl)) {
-                        return super.visitTag(tag, ctx);
-                    }
-                }
-                try {
-                    // Create new attributes list
-                    List<Xml.Attribute> newAttributes = new ArrayList<>(tag.getAttributes());
-                    // Create new href attribute
-                    String newHref = href.replace(oldUrl, newUrl);
-                    Xml.Tag newTag = Xml.Tag.build(newUrl);
-                    System.out.println("New tag created: {}" + newTag.getContent());
-                    System.out.println("***********************************************************************");
-                    return newTag;
-                } catch (Exception e) {
-                    System.out.println("Error modifying tag: " + e.getMessage());
-                    System.out.println("***********************************************************************");
-                    ctx.getOnError().accept(e);
-                    return tag;
-                }
-            }
-            private Optional<Xml.Attribute> findAttribute(Xml.Tag tag, String name) {
-                return tag.getAttributes().stream()
-                        .filter(attr -> name.equals(attr.getKeyAsString()))
-                        .findFirst();
-            }
-        };
-        return Preconditions.check(new FindSourceFiles("**/*.jsp"), xmlVisitor);
+    public PlainTextVisitor<ExecutionContext> getVisitor() {
+        return new LibraryResourceVisitor();
     }
 
-    /*
-    private static class LibraryResourceVisitor extends XmlVisitor<ExecutionContext> {
-        public LibraryResourceVisitor() {}
+    private static class LibraryResourceVisitor extends PlainTextVisitor<ExecutionContext> {
         @Override
         public boolean isAcceptable(SourceFile sourceFile, ExecutionContext ctx) {
             String path = sourceFile.getSourcePath().toString();
             boolean isJsp = path.endsWith(".jsp");
             boolean isInWebInf = path.contains("WEB-INF");
-            System.out.println("Checking file: " + path + " (isJsp: " + isJsp + ", isInWebInf: " + isInWebInf + ")");
+            if(isJsp && isInWebInf) {
+                System.out.println("Checking file: " + path + " (isJsp:" + isJsp + " , isInWebInf:" + isInWebInf + " )");
+            }
             return isJsp && isInWebInf;
         }
+
         @Override
-        public Xml visitTag(Xml.Tag tag, ExecutionContext ctx) {
-            libraryName = "bootstrap.css";
-            oldUrl = "<link rel=\"stylesheet\" href=\"<c:url value='webjars/bootstrap/3.3.7-1/css/bootstrap.min.css'/>\" />;";
-            newUrl = "<link rel=\"stylesheet\" type=\"text/css\" href=\"resources/librerias/bootstrap/3.3.7-1/css/bootstrap.min.css\" />;";
-            System.out.println("***********************************************************************");
-            System.out.println("Visiting tag: " + tag.getName() + " in " + getClass().getSimpleName());
-            String currentMarkup = "";
-            System.out.println("tag.getContent(): " + tag.getContent());
-            // Only process <link> tags
-            if (!"link".equals(tag.getName())) {
-                return super.visitTag(tag, ctx);
+        public @Nullable PlainText visitText(PlainText text, ExecutionContext ctx) {
+            System.out.println("Inside visitingText method");
+            String libraryName = "bootstrap.min.css";
+            String oldUrl1 = "<link rel=\"stylesheet\" href=\"<c:url value='webjars/bootstrap/3.3.7-1/css/bootstrap.min.css'/>\" />";
+            String oldUrl2 = "<link rel=\"stylesheet\" href=\"<c:url value='webjars/bootstrap/3.3.7-1/css/bootstrap.min.css'/>\"/>";
+            String newUrl = "<link rel=\"stylesheet\" type=\"text/css\" href=\"resources/librerias/bootstrap/3.3.7-1/css/bootstrap.min.css\" />";
+            System.out.println("Processing file: " + getCursor().getPath());
+            String content = text.getText();
+            // Check if the file contains our target
+            if (!content.contains(libraryName)) {
+                return text;
             }
-            // Get the href attribute
-            Optional<Xml.Attribute> hrefAttr = findAttribute(tag, "href");
-            if (!hrefAttr.isPresent()) {
-                return super.visitTag(tag, ctx);
-            }
-            String href = hrefAttr.get().getValueAsString();
-            System.out.println("Found href: " + href);
-            // Check if this is our target library
-            if (!href.contains(libraryName)) {
-                return super.visitTag(tag, ctx);
-            }
-            System.out.println("Found matching library reference");
-            if(tag.getContent().contains("href") && tag.getContent().contains(libraryName)){
-                currentMarkup = tag.getContent().toString();
-                System.out.println("currentMarkup: " + currentMarkup.toString());
-                System.out.println("Modifying tag...");
-                if (!currentMarkup.contains(oldUrl)) {
-                    return super.visitTag(tag, ctx);
+            System.out.println("Found file containing library reference");
+            // Find all link tags containing our library
+            String[] lines = content.split("\n");
+            StringBuilder newContent = new StringBuilder();
+            boolean madeChanges = false;
+            // Find all link tags containing our library
+            for (String line : lines) {
+                if (line.contains("<link")  && line.contains(libraryName)) {
+                    System.out.println("Original line: ["+line+"]");
+                    System.out.println("Contains oldUrl1 pattern? " + line.contains(oldUrl1));
+                    System.out.println("Contains oldUrl2 pattern? " + line.contains(oldUrl2));
+                    // Create new line with replacement
+                    String newLine = line;
+                    // Try both trim and non-trim versions
+                    //String trimmedLine = line.trim();
+                    //System.out.println("Trimmed line: ["+trimmedLine+"]");
+                    if(line.contains(oldUrl1)) {
+                        newLine = line.replace(oldUrl1, newUrl);
+                    }else if(line.contains(oldUrl2)) {
+                        newLine = line.replace(oldUrl2, newUrl);
+                    }
+                    madeChanges = true;
+                    System.out.println("Modified line: ["+newLine+"]");
+                    newContent.append(newLine).append("\n");
+                } else {
+                    newContent.append(line).append("\n");
                 }
             }
-            try {
-                // Create new attributes list
-                List<Xml.Attribute> newAttributes = new ArrayList<>(tag.getAttributes());
-                // Create new href attribute
-                String newHref = href.replace(oldUrl, newUrl);
-                Xml.Tag newTag = Xml.Tag.build(newUrl);
-                System.out.println("New tag created: {}" + newTag.getContent());
-                System.out.println("***********************************************************************");
-                return newTag;
-            } catch (Exception e) {
-                System.out.println("Error modifying tag: " + e.getMessage());
-                System.out.println("***********************************************************************");
-                ctx.getOnError().accept(e);
-                return tag;
+            if (!madeChanges) {
+                return text;
             }
-        }
-        private Optional<Xml.Attribute> findAttribute(Xml.Tag tag, String name) {
-            return tag.getAttributes().stream()
-                    .filter(attr -> name.equals(attr.getKeyAsString()))
-                    .findFirst();
+            System.out.println("Changes made to file, returning modified content");
+            return text.withText(newContent.toString());
         }
     }
-     */
 }
+
