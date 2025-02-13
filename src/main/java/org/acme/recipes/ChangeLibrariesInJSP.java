256c05fe54b65559ebaa3d8cff07bad42eab84ac
Nuria Malet Quintar <maletq.pr@gmail.com>
2025-02-13 14:42:47 -0300

    adds parameterization for recipe

--- src/main/java/org/acme/recipes/ChangeLibrariesInJSP.java
+++ src/main/java/org/acme/recipes/ChangeLibrariesInJSP.java
@@ -1,5 +1,7 @@
 package org.acme.recipes;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
 import org.openrewrite.SourceFile;
@@ -8,6 +10,24 @@ import org.openrewrite.text.PlainTextVisitor;
 import org.openrewrite.text.PlainText;
 
 public class ChangeLibrariesInJSP extends Recipe {
+
+    private static String libraryName;
+    private static String oldUrl1;
+    private static String oldUrl2;
+    private static String newUrl;
+
+    // Recipe configuration is injected via the constructor
+    @JsonCreator
+    public ChangeLibrariesInJSP(
+            @JsonProperty("libraryName") String libraryName,
+            @JsonProperty("oldUrl1") String oldUrl1,
+            @JsonProperty("oldUrl2") String oldUrl2,
+            @JsonProperty("newUrl") String newUrl) {
+        this.libraryName = libraryName;
+        this.oldUrl1 = oldUrl1;
+        this.oldUrl2 = oldUrl2;
+        this.newUrl = newUrl;
+    }
 
     @Override
     public String getDisplayName() {
@@ -39,10 +59,7 @@ public class ChangeLibrariesInJSP extends Recipe {
         @Override
         public @Nullable PlainText visitText(PlainText text, ExecutionContext ctx) {
             System.out.println("Inside visitingText method");
-            String libraryName = "bootstrap.min.css";
-            String oldUrl1 = "<link rel=\"stylesheet\" href=\"<c:url value='webjars/bootstrap/3.3.7-1/css/bootstrap.min.css'/>\" />";
-            String oldUrl2 = "<link rel=\"stylesheet\" href=\"<c:url value='webjars/bootstrap/3.3.7-1/css/bootstrap.min.css'/>\"/>";
-            String newUrl = "<link rel=\"stylesheet\" type=\"text/css\" href=\"resources/librerias/bootstrap/3.3.7-1/css/bootstrap.min.css\" />";
+            System.out.println("libraryName: " + libraryName);
             String content = text.getText();
             // Check if the file contains our target
             if (!content.contains(libraryName)) {
@@ -57,13 +74,23 @@ public class ChangeLibrariesInJSP extends Recipe {
             for (String line : lines) {
                 if (line.contains("<link")  && line.contains(libraryName)) {
                     System.out.println("Original line: ["+line+"]");
-                    System.out.println("Contains oldUrl1 pattern? " + line.contains(oldUrl1));
-                    System.out.println("Contains oldUrl2 pattern? " + line.contains(oldUrl2));
                     // Create new line with replacement
                     String newLine = line;
-                    if(line.contains(oldUrl1)) {
+                    if(oldUrl1 != null && line.contains(oldUrl1)) {
                         newLine = line.replace(oldUrl1, newUrl);
-                    }else if(line.contains(oldUrl2)) {
+                    }else if(oldUrl2 != null && line.contains(oldUrl2)) {
+                        newLine = line.replace(oldUrl2, newUrl);
+                    }
+                    madeChanges = true;
+                    System.out.println("Modified line: ["+newLine+"]");
+                    newContent.append(newLine).append("\n");
+                } else if (line.contains("<script")  && line.contains(libraryName)) {
+                    System.out.println("Original line: ["+line+"]");
+                    // Create new line with replacement
+                    String newLine = line;
+                    if(oldUrl1 != null && line.contains(oldUrl1)) {
+                        newLine = line.replace(oldUrl1, newUrl);
+                    }else if(oldUrl2 != null && line.contains(oldUrl2)) {
                         newLine = line.replace(oldUrl2, newUrl);
                     }
                     madeChanges = true;
